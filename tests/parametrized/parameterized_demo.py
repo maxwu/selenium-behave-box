c3dd597c25f38961465a63c71a1195fefcd05f80
maxwu <maxwunj@gmail.com>
2017-08-10 23:43:27 +1200

    add parameterized demo

--- tests/parametrized/parameterized_demo.py
+++ tests/parametrized/parameterized_demo.py
@@ -1,9 +1,38 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Demo for parameterized test.
-
+"""Demo for parametrized test.
+Java parametrized sample is located at https://github.com/maxwu/minnetonka/tree/master/src/main/java/sample.
+These samples will be merged to a new separate repo.
 """
 
-if __name__ == "__main__":
-    pass
+from parameterized import parameterized
+from nose.tools import assert_equal
+import unittest
+
+
+def get_parameters():
+    # return list of list
+    return [
+        ("alpha", "a", "a"),
+        ("beta", "a", "b"),
+        ("gamma", "b", "b"),
+    ]
+
+
+class TestSequence(unittest.TestCase):
+    @parameterized.expand([
+        ("alpha", "a", "a"),
+        ("beta", "a", "b"),
+        ("gamma", "b", "b"),
+    ])
+    def test_sequence(self, name, a, b):
+        print "testing %s with (%s, %s)" %(name, a, b)
+        assert_equal(a, b)
+
+
+# if __name__ == "__main__":
+#     import nose
+#     import sys
+#     sys.argv += ['--with-xunit', '--with-html']
+#     nose.run()
